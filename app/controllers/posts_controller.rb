e11b5cf7e6f009aec071427e7795ca902a6863be
LeonhardBrenninkmeijer <leo.brenninkmeijer@kellogg.northwestern.edu>
2026-03-04 16:19:53 +0000

    Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    if @current_user == nil
+      flash["notice"] = "You must be logged in to create a post."
+      redirect_to "/login"
+    end
   end
-  
+
   def create
     @post = Post.new
     @post["body"] = params["body"]
     @post["image"] = params["image"]
-    # TODO: assign logged-in user as user that created the post
+    @post["user_id"] = @current_user["id"]
     @post.save
     redirect_to "/posts"
   end
